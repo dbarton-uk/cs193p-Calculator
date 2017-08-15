6be49515ae0b6e2b4191e0e204da261f6aeb38d6
David Barton <david@i4-software.co.uk>
2017-08-15 19:11:05 +0100

    Assignment 3, Required Task 6

---
+++ Calculator/GraphView.swift
@@ -0,0 +1,96 @@
+//
+//  GraphView.swift
+//  Calculator
+//
+//  Created by David Barton on 11/08/2017.
+//  Copyright © 2017 David Barton. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GraphView: UIView {
+    
+    @IBInspectable
+    var fx: (Double) -> Double = cos
+    
+    @IBInspectable
+    var pointsPerUnit: CGFloat = 20
+    
+    @IBInspectable
+    var colour: UIColor = UIColor.red
+    
+    private var origin: CGPoint {
+        return self.center
+    }
+    
+    private var originInPixels: CGPoint {
+        return scaleToPixel(fromPoint: origin)
+    }
+    
+    private func scaleToPixels(fromPoints: CGFloat) -> CGFloat {
+        return fromPoints * contentScaleFactor
+    }
+    
+    private func scaleToPixel(fromPoint: CGPoint) -> CGPoint {
+        return CGPoint(x: scaleToPixels(fromPoints: fromPoint.x), y: scaleToPixels(fromPoints: fromPoint.y))
+        
+    }
+    
+    private func drawAxes() {
+
+    }
+    
+    private func pathForFunction(in rect: CGRect) -> UIBezierPath {
+        
+        func convertToUnitX(forPixel x: CGFloat) -> CGFloat {
+            return (x - originInPixels.x) / (pointsPerUnit * contentScaleFactor)
+        }
+        
+        func convertToPixelY(forUnit y: CGFloat) -> CGFloat {
+            return (y * pointsPerUnit * -1) + originInPixels.y
+        }
+        
+        func calculatePixelY(forPixel x: CGFloat) -> CGFloat {
+            
+            let ux = convertToUnitX(forPixel: x)
+            let uy = CGFloat(fx(Double(ux)))
+            
+            return convertToPixelY(forUnit: uy)
+        }
+        
+        func calculatePoint(forPixel x: CGFloat) -> CGPoint {
+            let y = calculatePixelY(forPixel: x)
+            return scaleToPoint(pixel: CGPoint(x: x, y: y))
+        }
+        
+        func scaleToPoint(_ value: CGFloat) -> CGFloat {
+            return value / contentScaleFactor
+        }
+        
+        func scaleToPoint(pixel: CGPoint) -> CGPoint {
+            return CGPoint(x: scaleToPoint(pixel.x), y: scaleToPoint(pixel.y))
+        }
+        
+        let widthInPixels = scaleToPixels(fromPoints: rect.width)
+        let path = UIBezierPath()
+        
+        path.move(to: calculatePoint(forPixel: 0))
+        
+        for x in stride(from: 1, through: widthInPixels, by: 1) {
+            path.addLine(to: calculatePoint(forPixel: x))
+        }
+        
+        return path
+    }
+    
+    
+    override func draw(_ rect: CGRect) {
+
+        AxesDrawer(contentScaleFactor: self.contentScaleFactor).drawAxes(in: bounds, origin: origin, pointsPerUnit: pointsPerUnit)
+        colour.set()
+        pathForFunction(in: rect).stroke();
+
+    }
+
+}
---
+++ Calculator/GraphViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GraphViewController.swift
+//  Calculator
+//
+//  Created by David Barton on 11/08/2017.
+//  Copyright © 2017 David Barton. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+
+}
